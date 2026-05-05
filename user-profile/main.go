1c3dbe09e096442cb1025d4635571442a66ae156
tonah <onahpatience1610@gmail.com>
2026-05-05 10:01:42 +0100

    modified the user.go file

--- user-profile/main.go
+++ user-profile/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("🏌️ Welcome to Golf Club Membership Checker")
+	fmt.Println("🏌️ Welcome to Lagos Golf Club Membership Checker")
 
 	user := getUserProfile()
 	checkMembership(user)
--- user-profile/user.go
+++ user-profile/user.go
@@ -8,31 +8,33 @@ import (
 type User struct {
 	FullName string
 	Email    string
-	Phone    float64
+	Phone    string
 }
 
-func getUserProfile() User {
-	var firstName string
-	var lastName string
-	var phone float64
+var users []User
 
-	fmt.Print("Enter first name: ")
+func createUserFlow() {
+	var firstName, lastName, phone string
+
+	fmt.Print("First name: ")
 	fmt.Scanln(&firstName)
 
-	fmt.Print("Enter last name: ")
+	fmt.Print("Last name: ")
 	fmt.Scanln(&lastName)
 
-	fmt.Print("Enter phone number (numeric): ")
+	fmt.Print("Phone: ")
 	fmt.Scanln(&phone)
 
 	fullName := firstName + " " + lastName
-	firstInitial := string(firstName[0])
+	email := strings.ToLower(string(firstName[0]) + lastName + "@01edu.net")
 
-	email := strings.ToLower(firstInitial + lastName) + "@01edu.net"
-
-	return User{
+	user := User{
 		FullName: fullName,
 		Email:    email,
 		Phone:    phone,
 	}
+
+	users = append(users, user)
+
+	fmt.Println("User created successfully!")
 }
